0a01a1fe5d64664e15374dc7309e3cfa53f0f668
Abeer <khanghauriabeer@gmail.com>
2025-11-14 12:10:55 +0530

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,15 @@
+a=int(input("NO.1 = "))
+b=int(input("NO.2 = "))
+choice=input("enter your choice +,-,*,/")
+
+if choice=="+":
+    print(a+b)
+elif choice=="-":
+        print(a-b)
+elif choice=="*":
+        print(a*b)
+elif choice=="/":
+            print(a/b)
+else:
+                print("invalid choice")
+        
